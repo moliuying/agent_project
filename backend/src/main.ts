c3dfec3a75f2d780785745f7a990249ab2dce6a2
雷朝军 <chaojun.lei@naxions.com>
2026-06-06 21:16:14 +0800

    功能升级

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
-  console.log('Backend server is running on http://localhost:3000');
+  await app.listen(4000);
+  console.log('Backend server is running on http://localhost:4000');
 }
 
 bootstrap();
